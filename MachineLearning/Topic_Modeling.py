911c7a04a6da348e1ce60917a01f804b52173c64
kinetiz <thanadon.f@gmail.com>
2018-04-29 18:46:12 +0100

    try transform doc and check which topic it belongs to

--- MachineLearning/Topic_Modeling.py
+++ MachineLearning/Topic_Modeling.py
@@ -16,12 +16,21 @@ no_features = 1000
 tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
 tfidf = tfidf_vectorizer.fit_transform(documents)
 tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+#idf of all words
+tfidf_vectorizer.idf_
+#check vocab -> count of all words
+tfidf_vectorizer.vocabulary_
+#vector of document 1. in term weight form
+print(tfidf[1])
 
 # LDA can only use raw term counts for LDA because it is a probabilistic graphical model
 tf_vectorizer = CountVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
 tf = tf_vectorizer.fit_transform(documents)
 tf_feature_names = tf_vectorizer.get_feature_names()
-
+#check vocab -> count of all words
+tf_vectorizer.vocabulary_
+#vector of document 1. in term frequency (word count form
+print(tf[1])
 no_topics = 20
 
 # Run NMF
@@ -31,5 +40,18 @@ nmf = NMF(n_components=no_topics, random_state=1, alpha=.1, l1_ratio=.5, init='n
 lda = LatentDirichletAllocation(n_topics=no_topics, max_iter=5, learning_method='online', learning_offset=50.,random_state=0).fit(tf)
 
 no_top_words = 10
+
 display_topics(nmf, tfidf_feature_names, no_top_words)
 display_topics(lda, tf_feature_names, no_top_words)
+
+nmf.components_.shape
+import numpy
+#check which topic the document belong to
+doc1result = nmf.transform(tfidf[2])
+maxTopicProb = numpy.max(doc1result)
+print("maxTopicProbability: " + str(maxTopicProb))
+itemindex = numpy.where(doc1result==maxTopicProb)
+print("This doc belong to topic: "+str(itemindex[1][0]))
+#Doc2 belong to topic13 :"Although I realize that principle is not one of your strongest\npoints, I would still like to know why do do not ask any question\nof this sort about the Arab countries.\n\n   If you want to continue this think tank charade of yours, your\nfixation on Israel must stop.  You might have to start asking the\nsame sort of questions of Arab countries as well.  You realize it\nwould not work, as the Arab countries' treatment of Jews over the\nlast several decades is so bad that your fixation on Israel would\nbegin to look like the biased attack that it is.\n\n   Everyone in this group recognizes that your stupid 'Center for\nPolicy Research' is nothing more than a fancy name for some bigot\nwho hates Israel."
+#Topic13: like sounds looks look bike sound lot things really thing
+documents[2]
