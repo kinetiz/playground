d8b392b45f506ee5796d391a16f762974e2964b6
kinetiz <thanadon.f@gmail.com>
2018-06-10 22:53:08 +0100

    Neo4j MERGE POC
    Testing MERGE of CQL - Neo4j query + use neo4j native driver

--- Neo4j/poc_neo4j.py
+++ Neo4j/poc_neo4j.py
@@ -31,19 +31,23 @@ RETURN tweets.tid, tweets.created_at, hashtag"""
 #     ON CREATE SET rh.count = 1
 #     ON MATCH  SET rh.count = coalesce(rh.count, 0)+1
 #     """
-
 import json
 from py2neo import Graph, authenticate
+from neo4j.v1 import GraphDatabase
+import os
+import logging
+import datetime
 
-# Connect to neo4j
-authenticate("localhost:7474", "neo4j", "11111")
-graph = Graph()
+### Setup logging
+logging.basicConfig(filename='G:\\work\\TwitterAPI\\Neo2\\insert_history.log',level=logging.INFO, format='%(asctime)s.%(msecs)03d : %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+
 ## data
 data = {"tweets": [
     {
         "hashtags": [
             "bitcoin",
             "ethereum",
+            "Bitcoin",
             "ico",
             "binance",
             "xoxo"
@@ -75,7 +79,7 @@ data = {"tweets": [
             "Cryptocurrency",
             "Blockchain",
             "ToTheMOON",
-            "Bitcoin"
+            "xoxo"
         ],
         "tweet": "#HappyWeekend to all the #crypto community ",
         "tid": 995453565689790466,
@@ -97,42 +101,121 @@ data = {"tweets": [
 ]
 }
 
+# Connect to database
+uri = "bolt://localhost:7687"
+driver = GraphDatabase.driver(uri, auth=("neo4j", "11111"))
 
-with open("G:/work/TwitterAPI/data/top1/test.json") as f:
-    data = json.load(f)
+# create nodes
+# with driver.session() as session:
+#     tx = session.begin_transaction()
+#     for t in data['tweets']:
+#         for h in t['hashtags']:
+#             # Open transaction
+#             q0 = """ Create (h1:Hashtag{tag: $tag1})"""
+#             ret = tx.run(q0, tag1=h)
+#             # ret = tx.run(q, tag1=hashtag[i], tag2=hashtag[j])
+#     tx.commit()
 
 
-q_build_initial_graph = """WITH {json} AS document
-    UNWIND document.tweets AS tweets
-    UNWIND tweets.hashtags AS hashtag
-    //Tweet
-    MERGE (t:Tweet {id: tweets.tid})
-        ON CREATE SET 
-            t.id = tweets.tid,
-            t.text = tweets.tweet,    
-            t.created_at = tweets.created_at,
-            t.country = tweets.country,
-            t.link_count = tweets.link_count
-    //Users
-    MERGE (u:User {id: tweets.uid})
-        ON CREATE SET 
-            u.id = tweets.uid,
-            u.screen_name = tweets.screen_name,
-            u.follower_count = tweets.friends_count,
-            u.following_count = tweets.followers_count  
-    //Hashtag
-    MERGE (h:Hashtag {tag: toLower(hashtag)})
-        ON CREATE SET 
-            h.tag = toLower(hashtag)      
+## test json import
+with driver.session() as session:
+    # Open transaction
+    tx = session.begin_transaction()
 
-    //Tweet->Hashtag
-    MERGE (t)-[:has]->(h)
-    
-    //User->Tweet
-    MERGE (u)-[:post]->(t)
-    
-    RETURN count(t)
-    """
-q = q_build_initial_graph
-graph.run(q, json=data).dump()
+    q = """WITH {json} AS document
+                UNWIND document.tweets AS tweets
+                UNWIND tweets.hashtags AS hashtag
+                //Hashtag
+                MERGE (h:Hashtag {tag: hashtag})
+                    ON CREATE SET
+                        h.tag = hashtag
+                RETURN count(h)
+                """
+    ret = tx.run(q, json=data).single().value()
+    print(ret)
+    # ret = tx.run(q, tag1=hashtag[i], tag2=hashtag[j])
+    tx.commit()
+
+    # Open transaction
+    tx = session.begin_transaction()
+
+    q = """WITH {json} AS document
+                    UNWIND document.tweets AS tweets
+                    UNWIND tweets.hashtags AS hashtag
+                    //Hashtag
+                    MERGE (h:Hashtag {tag: hashtag})
+                        ON CREATE SET
+                            h.tag = hashtag
+                    RETURN count(h)
+                    """
+    ret = tx.run(q, json=data).single().value()
+    print(ret)
+    # ret = tx.run(q, tag1=hashtag[i], tag2=hashtag[j])
+    tx.commit()
+
+
+# add hash-hash relationship
+with driver.session() as session:
+    # Open transaction
+    tx = session.begin_transaction()
+    for t in data['tweets']:
+        hashtag = t['hashtags']
+        hashLen = len(hashtag)
+        # Loop matching hashtag but not include previous eg. 3 tags -> (1,2),(1,3),(2,3) -> not include 2,1 /2,2 / 3,2
+        for i in range(0, hashLen - 1):
+            for j in range(i + 1, hashLen):
+                q = """ MATCH (h1:Hashtag{tag: $tag1}),
+                              (h2:Hashtag{tag: $tag2})
+                        MERGE (h1)-[r:together]-(h2)
+                        ON CREATE SET r.count = 1
+                        ON MATCH SET r.count = coalesce(r.count,0) + 1
+                        RETURN count(r)"""
+                ret = tx.run(q, tag1=hashtag[i], tag2=hashtag[j]).single().value()
+    tx.commit()
+print(str(ret)+" Hash-hash links added..")
+logging.info(str(ret)+" Hash-hash links added..")
+
+# with open("G:/work/TwitterAPI/data/top1/test.json") as f:
+#     data = json.load(f)
+
+#
+# q = """ MERGE (h1:Hashtag {tag: hh1})-(r:together)-(h2:Hashtag {tag: hh2})
+#           ON CREATE SET r.count = 1
+#           ON MATCH SET
+#             r.count = coalesce(r.count, 0) + 1 """;
+#
+# q_build_initial_graph = """WITH {json} AS document
+#     UNWIND document.tweets AS tweets
+#     UNWIND tweets.hashtags AS hashtag
+#     //Tweet
+#     MERGE (t:Tweet {id: tweets.tid})
+#         ON CREATE SET
+#             t.id = tweets.tid,
+#             t.text = tweets.tweet,
+#             t.created_at = tweets.created_at,
+#             t.country = tweets.country,
+#             t.link_count = tweets.link_count
+#     //Users
+#     MERGE (u:User {id: tweets.uid})
+#         ON CREATE SET
+#             u.id = tweets.uid,
+#             u.screen_name = tweets.screen_name,
+#             u.follower_count = tweets.friends_count,
+#             u.following_count = tweets.followers_count
+#     //Hashtag
+#     MERGE (h:Hashtag {tag: toLower(hashtag)})
+#         ON CREATE SET
+#             h.tag = toLower(hashtag)
+#
+#     //Tweet->Hashtag
+#     MERGE (t)-[:has]->(h)
+#
+#     //User->Tweet
+#     MERGE (u)-[:post]->(t)
+#
+#     RETURN count(t)
+#     """
+# q = q_build_initial_graph
+# a = graph.run(q, json=data).dump()
+# print(a)
 
